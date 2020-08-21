18047831a8db9f6c879aa0d69781e4119b553a70
BillChenJava <58630964+BillChenJava@users.noreply.github.com>
2020-08-21 11:36:13 -0400

    test

--- src/main/java/com/mcd/springmongo/controller/UserController.java
+++ src/main/java/com/mcd/springmongo/controller/UserController.java
@@ -29,7 +29,6 @@ public class UserController {
 
     @PostMapping(value = "")
     public User addNewUser(@RequestBody User user){
-        System.out.println("test123");
         return userRepository.save(user);
     }
 
